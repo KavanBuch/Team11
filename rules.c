2ae5dfd5df947e9a1b738fdc9d5e668b42736f4c
kavan02 <kavanbuch02@gmail.com>
2022-04-25 20:31:17 +0530

    Ready with the basic prototype

--- rules.c
+++ rules.c
@@ -33,14 +33,49 @@ int main()
     printf("*********************Welcome to Team11*********************\n\n");
     printf("Following are the rules for making your team:\n\n");
     reset();
+
+    green();
+
     printf("You will be given a list of 30 available players from both of the teams(15 from each).You are supposed to choose any 11 out of them with some bounds as follows:\n\n");
     printf("(1)Total team size should be 11 and maximum of 7 players can be chosen from one team.\n\n");
     printf("(2)Your team can different combination of players.\n\n");
-    printf("(3)Your team must follow the following criteria:\n");
-    printf("\t(i)  Minimum of 1 and maximum of 4 wicket-keeper should be selected.\n");
-    printf("\t(ii) Minimum of 3 and maximum of 6 batsman should be selected.\n");
-    printf("\t(iii)Minimum of 1 and maximum of 4 all-rounder should be selected.\n");
-    printf("\t(iv) Minimum of 3 and maximum of 6 bowler should be selected.\n");
     printf("\n");
+
+    red();
+    printf("You have following choices:\n\n");
+    printf("From Team1:\n\n");
+    purple();
+    printf("Michael Hussey\n"
+           "Murali Vijay\n"
+           "Suresh Raina\n"
+           "S Badrinath\n"
+           "MS Dhoni\n"
+           "Albie Morkel\n"
+           "Dwayne Bravo\n"
+           "Wriddhiman Saha\n"
+           "Ravichandran Ashwin\n"
+           "Doug Bollinger\n"
+           "Shadab Jakati\n"
+           "Deepak Chahar\n"
+           "Shardul Thakur\n"
+           "Josh Hazalwood\n");
+    red();
+    printf("\nFrom Team2:\n\n");
+    purple();
+    printf("Davy Jacob\n"
+           "Aiden Blizzard\n"
+           "Tirumalasetti Suman\n"
+           "Ambati Rayudu\n"
+           "Andrew Symonds\n"
+           "Kieron Pollard\n"
+           "Rajagopal Sathish\n"
+           "Harbhajan Singh\n"
+           "Lasith Malinga\n"
+           "Abu Nechim\n"
+           "Yuzvendra Chahal\n"
+           "Rohit Sharma\n"
+           "Ishan Kishan\n"
+           "Dwayne Smith\n");
+    reset();
     return 0;
 }
---
+++ user_input.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define TEAM_SIZE 31
+#define DATA_SIZE 100
+
+void red()
+{
+    printf("\033[1;31m");
+}
+void green()
+{
+    printf("\033[0;32m");
+}
+void yellow()
+{
+    printf("\033[1;33m");
+}
+void blue()
+{
+    printf("\033[0;34m");
+}
+void purple()
+{
+    printf("\033[0;35m");
+}
+void reset()
+{
+    printf("\033[0m");
+}
+
+char *players[TEAM_SIZE] = {"Michael Hussey ", "Murali Vijay ", "Suresh Raina", "S Badrinath", "MS Dhoni", "Albie Morkel", "Dwayne Bravo", "Wriddhiman Saha", "Ravichandran Ashwin", "Doug Bollinger", "Shadab Jakati", "Deepak Chahar", "Shardul Thakur", "Josh Hazalwood", "Davy Jacob", "Aiden Blizzard", "Tirumalasetti Suman", "Ambati Rayudu", "Andrew Symonds", "Kieron Pollard", "Rajagopal Sathish", "Harbhajan Singh", "Lasith Malinga", "Abu Nechim", "Yuzvendra Chahal", "Rohit Sharma", "Ishan Kishan", "Dwayne Smith"};
+
+int main()
+{
+    yellow();
+    int team[TEAM_SIZE];
+    for (int i = 1; i < TEAM_SIZE; i++)
+    {
+        team[i] = 0;
+    }
+
+    for (int i = 0; i < 11; i++)
+    {
+        printf("Enter your team players's number(between 1 to 30):\t ");
+        int player_no;
+        scanf("%d", &player_no);
+        team[player_no] = 1;
+    }
+    reset();
+
+    int sum1 = 0, sum2 = 0;
+    for (int i = 1; i < 16; i++)
+    {
+        sum1 += team[i];
+    }
+    for (int i = 16; i < 31; i++)
+    {
+        sum2 += team[i];
+    }
+
+    int error = 0;
+    for (int i = 1; i < TEAM_SIZE; i++)
+    {
+        if (team[i] > 1)
+        {
+            error = 1;
+        }
+    }
+
+    if (sum1 > 7 || sum2 > 7)
+    {
+        red();
+        printf("You cannot select more than 7 players from one team\n\n");
+        reset();
+    }
+    else if (error == 1)
+    {
+        red();
+        printf("No player can be selected more than once\n\n");
+        reset();
+    }
+    else
+    {
+        FILE *input_file;
+        input_file = fopen("client_team.txt", "w");
+        if (input_file == NULL)
+        {
+            printf("failed to open.\n");
+        }
+        else
+        {
+            for (int i = 1; i < TEAM_SIZE; i++)
+            {
+                if (team[i] == 1)
+                {
+                    fputs(players[i], input_file);
+                    fputs("\n", input_file);
+                }
+            }
+        }
+        fclose(input_file);
+    }
+    return 0;
+}
